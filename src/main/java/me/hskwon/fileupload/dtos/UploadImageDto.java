c9e47265c84aaa0355453eec9545ba9bb5395fe2
hoeseong <hoeseong19@naver.com>
2023-12-06 01:25:28 +0900

    feat: UploadImageDto에 username 추가

--- src/main/java/me/hskwon/fileupload/dtos/UploadImageDto.java
+++ src/main/java/me/hskwon/fileupload/dtos/UploadImageDto.java
@@ -3,6 +3,7 @@ package me.hskwon.fileupload.dtos;
 import org.springframework.web.multipart.MultipartFile;
 
 public record UploadImageDto(
+        String username,
         MultipartFile image
 ) {
 }
